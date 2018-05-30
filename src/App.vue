0c572c1d8f70949639d7933c2862aadcc241224c
Mashiro <adadam@qq.com>
2018-05-30 23:50:49 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main">
+    <div id="app">
+      <img src="./assets/image/logo.png">
+    <VuePerfectScrollbar id="scroll-container" v-once :settings="settings" @ps-scroll-y="scrollHanle">
+      
+      <HelloWorld msg="Hi, Mashiro!" />
+      <LinkItems />
+    </VuePerfectScrollbar>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue';
+  import LinkItems from './components/LinkItems.vue';
+  import VuePerfectScrollbar from 'vue-perfect-scrollbar';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app', // Just a name...?
+    components: {
+      HelloWorld,
+      LinkItems,
+      VuePerfectScrollbar
+    },
+    data() {
+      return {
+        settings: {
+          wheelSpeed: 0.5,
+          wheelPropagation: true,
+          minScrollbarLength: 5
+        }
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html {
+    padding: 0;
+    margin: 0;
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+  }
+
+  body {
+    background-image: url(./assets/image/bg.png);
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
+
+  #main {
+    margin: 5%;
+    height: 80%;
+  }
+
+  #app {
+    font-family: 'Comic Sans', cursive;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background-color: rgba(255, 255, 255, 0.7);
+    width: auto;
+    max-width: 800px;
+    margin: auto;
+    display: block;
+    height: 100%;
+    overflow: hidden;
+    border-radius: 10px
+  }
+
+  #app img {
+    margin-top: 20px;
+  }
+
+  #app #scroll-container {
+    width: 100%;
+    height: 85%;
+  }
 </style>
---
+++ src/components/LinkItems.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="hello">
+        <h3>Mine</h3>
+        <ul>
+            <li>
+                <a href="https://2heng.xin/wp-admin/" target="_blank">Dashboard</a>
+            </li>
+            <li>
+                <a href="https://sufe.ml/" target="_blank">Dasktop</a>
+            </li>
+            <li>
+                <a href="https://sufe.ml/index.php?share/folder&amp;user=1&amp;sid=PfmDQTSF" target="_blank">Read Online</a>
+            </li>
+            <li>
+                <a href="https://cloud.moezx.cc/" target="_blank">File Host</a>
+            </li>
+            <li>
+                <a href="https://view.moezx.cc/" target="_blank">Image Host</a>
+            </li>
+            <li>
+                <a href="https://music.shino.cc/" target="_blank">Download Music</a>
+            </li>
+            <li>
+                <a href="https://google.eriri.ml/" target="_blank">Google</a>
+            </li>
+            <li>
+                <a href="https://zh-wiki.eriri.ml/" target="_blank">Wiki</a>
+            </li>
+        </ul>
+
+        <h3>Other</h3>
+        <ul>
+            <li>
+                2015111846 <a href="http://bb.sufe.edu.cn/" target="_blank">bb</a></li>
+            <li>
+                <a href="https://dc.console.aliyun.com/dns/" target="_blank">Aliyun DNS</a>
+            </li>
+            <li>
+                <a href="https://cf.moezx.cc/" target="_blank">CF DNS</a>
+            </li>
+            <li>
+                <a href="http://www.iciba.com/" target="_blank">iciba</a>
+            </li>
+            <li>
+                <a href="https://translate.google.cn/" target="_blank">Translate</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: orange;
+    }
+</style>
